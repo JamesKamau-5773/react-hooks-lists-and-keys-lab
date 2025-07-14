5af97f8d396305afdad54a9e1b8f5663c59156bf
James Kamau <jamesnk5773@gmail.com>
2025-07-14 17:03:55 +0300

    keys

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,10 +6,15 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((technologies,index)=>{
+          return <span key= {index}>{technologies}</span>
+        })}
+          
+                
+        
       </div>
     </div>
   );
-}
+} console.log(ProjectItem)
 
 export default ProjectItem;
